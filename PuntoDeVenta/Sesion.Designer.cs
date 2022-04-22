9fdbce77db5c981faf53d51790c98a7d5175a75b
BenjaminCorona <benja13lara@gmail.com>
2022-04-22 17:10:06 -0500

    Prueba de inserción de cambios

--- PuntoDeVenta/Sesion.Designer.cs
+++ PuntoDeVenta/Sesion.Designer.cs
@@ -56,9 +56,10 @@ namespace PuntoDeVenta
             this.panel1.Controls.Add(this.BtnIniciar);
             this.panel1.Controls.Add(this.TxtUsuario);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(142, 129);
+            this.panel1.Location = new System.Drawing.Point(213, 198);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(364, 322);
+            this.panel1.Size = new System.Drawing.Size(546, 495);
             this.panel1.TabIndex = 0;
             // 
             // LblRegistro
@@ -67,9 +68,10 @@ namespace PuntoDeVenta
             this.LblRegistro.Cursor = System.Windows.Forms.Cursors.Hand;
             this.LblRegistro.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblRegistro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
-            this.LblRegistro.Location = new System.Drawing.Point(26, 233);
+            this.LblRegistro.Location = new System.Drawing.Point(39, 358);
+            this.LblRegistro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblRegistro.Name = "LblRegistro";
-            this.LblRegistro.Size = new System.Drawing.Size(114, 16);
+            this.LblRegistro.Size = new System.Drawing.Size(169, 24);
             this.LblRegistro.TabIndex = 7;
             this.LblRegistro.Text = "Crear un usuario";
             this.LblRegistro.Click += new System.EventHandler(this.LblRegistro_Click);
@@ -77,9 +79,10 @@ namespace PuntoDeVenta
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(234)))), ((int)(((byte)(234)))));
-            this.panel2.Location = new System.Drawing.Point(29, 50);
+            this.panel2.Location = new System.Drawing.Point(44, 77);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(275, 1);
+            this.panel2.Size = new System.Drawing.Size(412, 2);
             this.panel2.TabIndex = 6;
             // 
             // label3
@@ -87,9 +90,10 @@ namespace PuntoDeVenta
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
-            this.label3.Location = new System.Drawing.Point(26, 142);
+            this.label3.Location = new System.Drawing.Point(39, 218);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.Size = new System.Drawing.Size(119, 24);
             this.label3.TabIndex = 5;
             this.label3.Text = "Contraseña";
             // 
@@ -99,11 +103,12 @@ namespace PuntoDeVenta
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
-            this.textBox1.Location = new System.Drawing.Point(29, 161);
+            this.textBox1.Location = new System.Drawing.Point(44, 248);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(272, 43);
+            this.textBox1.Size = new System.Drawing.Size(407, 65);
             this.textBox1.TabIndex = 4;
             // 
             // label2
@@ -111,11 +116,12 @@ namespace PuntoDeVenta
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
-            this.label2.Location = new System.Drawing.Point(26, 66);
+            this.label2.Location = new System.Drawing.Point(39, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 16);
+            this.label2.Size = new System.Drawing.Size(27, 24);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Nombre de usuario";
+            this.label2.Text = "Id";
             // 
             // BtnIniciar
             // 
@@ -127,9 +133,10 @@ namespace PuntoDeVenta
             this.BtnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnIniciar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnIniciar.ForeColor = System.Drawing.Color.White;
-            this.BtnIniciar.Location = new System.Drawing.Point(0, 252);
+            this.BtnIniciar.Location = new System.Drawing.Point(0, 387);
+            this.BtnIniciar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnIniciar.Name = "BtnIniciar";
-            this.BtnIniciar.Size = new System.Drawing.Size(364, 70);
+            this.BtnIniciar.Size = new System.Drawing.Size(546, 108);
             this.BtnIniciar.TabIndex = 2;
             this.BtnIniciar.Text = "Iniciar";
             this.BtnIniciar.UseVisualStyleBackColor = false;
@@ -141,10 +148,11 @@ namespace PuntoDeVenta
             this.TxtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.TxtUsuario.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
-            this.TxtUsuario.Location = new System.Drawing.Point(29, 85);
+            this.TxtUsuario.Location = new System.Drawing.Point(44, 131);
+            this.TxtUsuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TxtUsuario.Multiline = true;
             this.TxtUsuario.Name = "TxtUsuario";
-            this.TxtUsuario.Size = new System.Drawing.Size(272, 43);
+            this.TxtUsuario.Size = new System.Drawing.Size(407, 65);
             this.TxtUsuario.TabIndex = 1;
             // 
             // label1
@@ -152,9 +160,10 @@ namespace PuntoDeVenta
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
-            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 22);
+            this.label1.Size = new System.Drawing.Size(233, 34);
             this.label1.TabIndex = 0;
             this.label1.Text = "Inicio de sesión";
             // 
@@ -166,9 +175,10 @@ namespace PuntoDeVenta
             this.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnCerrar.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnCerrar.ForeColor = System.Drawing.Color.Silver;
-            this.BtnCerrar.Location = new System.Drawing.Point(578, 1);
+            this.BtnCerrar.Location = new System.Drawing.Point(867, 2);
+            this.BtnCerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnCerrar.Name = "BtnCerrar";
-            this.BtnCerrar.Size = new System.Drawing.Size(40, 40);
+            this.BtnCerrar.Size = new System.Drawing.Size(60, 62);
             this.BtnCerrar.TabIndex = 1;
             this.BtnCerrar.Text = "X";
             this.BtnCerrar.UseVisualStyleBackColor = true;
@@ -176,15 +186,16 @@ namespace PuntoDeVenta
             // 
             // Sesion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(619, 616);
+            this.ClientSize = new System.Drawing.Size(928, 948);
             this.Controls.Add(this.BtnCerrar);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MinimumSize = new System.Drawing.Size(614, 616);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(921, 948);
             this.Name = "Sesion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
