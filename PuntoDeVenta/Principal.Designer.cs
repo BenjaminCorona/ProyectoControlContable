5f2bebcfa0b087183337509fc073191559e74fa8
EnriqueMA-jar <yahirmart03@gmail.com>
2022-04-23 20:41:57 -0500

    (1) Diseño Principal.cs

--- PuntoDeVenta/Principal.Designer.cs
+++ PuntoDeVenta/Principal.Designer.cs
@@ -32,6 +32,7 @@ namespace PuntoDeVenta
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -59,14 +60,24 @@ namespace PuntoDeVenta
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(129, 131);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Administrar productos";
+            this.button3.Text = "Consultar productos";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(57, 386);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 131);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Administrar proveedores";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1236, 786);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -83,5 +94,6 @@ namespace PuntoDeVenta
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
